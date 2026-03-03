5f46f0b1f20f2e99c3d9c030e29ad6b62c04daa2
farmer0010 <zmqkf16@naver.com>
2026-03-02 21:27:29 -0500

    fix: test

---
+++ srcs/requirements/mariadb/tools/create_db.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cat << EOF > /tmp/create_db.sql
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE} CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED by '${MYSQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
+FLUSH PRIVILEGES;
+EOF
+
+/usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+rm -f /tmp/create_db.sql
